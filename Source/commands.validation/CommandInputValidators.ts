616ebba2fd2ce4245f5deadf6e89eb15b036a493
Einar Ingebrigtsen <einar@dolittle.com>
2020-01-12 08:57:31 +0100

    Introducing a way to clear validators and types

--- Source/commands.validation/CommandInputValidators.ts
+++ Source/commands.validation/CommandInputValidators.ts
@@ -4,9 +4,12 @@
 import { CommandInputValidatorFor, CommandInputValidationResult, ICommand, ICommandInputValidators } from './index';
 import { RuleSetContainerEvaluation } from '@dolittle/rules';
 
+/**
+ * Represents an implementation of {ICommandInputValidators}
+ */
 export class CommandInputValidators implements ICommandInputValidators {
-    static _inputValidatorTypes: any = {};
-    static _ruleSetContainerEvaluationPerCommand: any = {};
+    private static _inputValidatorTypes: any = {};
+    private static _ruleSetContainerEvaluationPerCommand: any = {};
 
     /** @inheritdoc */
     validate(command: ICommand) {
@@ -25,6 +28,19 @@ export class CommandInputValidators implements ICommandInputValidators {
         return CommandInputValidators._ruleSetContainerEvaluationPerCommand.hasOwnProperty(commandType.prototype);
     }
 
+    /**
+     * Clear out any validators registered
+     */
+    static clear() {
+        CommandInputValidators._inputValidatorTypes = {};
+        CommandInputValidators._ruleSetContainerEvaluationPerCommand = {};
+    }
+
+    /**
+     * Register a validator type for a type of command in the system.
+     * @param {TCommand extends ICommand} commandType - The type of command to register.
+     * @param {*} validatorType - The validator type - must be something that extends {CommandInputValidatorFor<TCommand>} with type of command given.
+     */
     static register<TCommand extends ICommand>(commandType: (new () => TCommand), validatorType: (new () => CommandInputValidatorFor<TCommand>)) {
         CommandInputValidators._inputValidatorTypes[commandType.prototype] = validatorType;
         const builder = new validatorType();
