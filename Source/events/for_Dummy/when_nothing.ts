c1b26fd840dc5f1ff51db184e8b92d849c4e999f
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-10 19:14:52 +0200

    Fixing build errors

--- Source/events/for_Dummy/when_nothing.ts
+++ Source/events/for_Dummy/when_nothing.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 describe('when nothing', () => {
-    it('should be fine', true.should.be.true);
+    it('should be fine', () => true.should.be.true);
 });
