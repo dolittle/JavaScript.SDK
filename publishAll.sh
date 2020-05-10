0576d6cd2fb6e2210d349a7e6e4f5d746f0442e7
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-10 18:55:39 +0200

    Publishing version 1.1.0

--- publishAll.sh
+++ publishAll.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "No version number supplied"
-    exit 1
-fi
+export VERSION=$(git tag --sort=-version:refname | head -1)
 
-echo "Publishing $1"
+MAJOR_VERSION=$(echo $VERSION | sed 's/v*\([0-9]*\).*$/\1/g')
+MINOR_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.\([0-9]*\).*$/\1/g')
+PATCH_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.[0-9]*.\([0-9]*\).*$/\1/g')
+PRE_RELEASE_TAG=$(echo $VERSION | sed 's/v*[0-9]*.[0.9]*.[0-9]-\([a-zA-Z]*\).*$/\1/g')
+BUILD_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.[0.9]*.[0-9]-[a-zA-Z]*.\([0-9]*\)/\1/g')
+
+[[ $PRE_RELEASE_TAG == $BUILD_VERSION ]] &&
+    PACKAGE_VERSION=$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION ||
+    PACKAGE_VERSION=$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PRE_RELEASE_TAG.$BUILD_VERSION
+
+echo "Git Version : " $VERSION
+echo "Major Version : " $MAJOR_VERSION
+echo "Minor Version : " $MINOR_VERSION
+echo "Patch Version : " $PATCH_VERSION
+echo "Build Version : " $BUILD_VERSION
+echo "Package Version : " $PACKAGE_VERSION
+
+VERSION=
+
+echo "Publishing $PACKAGE_VERSION"
 
 for f in Source/*; do
     pushd $PWD > /dev/null
     cd $f
     echo "Publishing $f"
-    yarn publish --new-version $1 --no-git-tag-version
+    yarn publish --new-version $PACKAGE_VERSION --no-git-tag-version
     popd > /dev/null
 done
 
 echo "Committing changed files"
 git add .
-git commit -m "Publishing version $1"
-
-echo "Adding tag for the version"
-
-git tag $1
+git commit -m "Publishing version $PACKAGE_VERSION"
