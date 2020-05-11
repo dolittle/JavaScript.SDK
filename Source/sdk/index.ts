979a1320b1eed60f1ae812d0b221e790df9b66c9
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-11 13:13:54 +0200

    Setting up protobuf converters

--- Source/sdk/index.ts
+++ Source/sdk/index.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export * from './Client';
+export { Client } from './Client';
+export { ClientBuilder } from './ClientBuilder';
+
+import '@dolittle/sdk.protobuf';
