ec148f290656ca4b57ec193bd016541851b5fef9
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-11 13:13:39 +0200

    Importing and exporting all the converters

--- Source/protobuf/index.ts
+++ Source/protobuf/index.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+export * from './GuidConverters';
+
 import './ArtifactConverters';
 import './ClaimsConverters';
-import './GuidConverters';
 import './VersionConverters';
