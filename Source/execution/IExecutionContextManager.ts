3ae5867cd212fea9fe6815cccdb4ac347f972edc
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-11 14:12:56 +0200

    JSDoc

--- Source/execution/IExecutionContextManager.ts
+++ Source/execution/IExecutionContextManager.ts
@@ -5,9 +5,20 @@ import { ExecutionContext } from './ExecutionContext';
 import { TenantId } from './TenantId';
 import { Claims } from './Claims';
 
+/**
+ * Defines a manager for working with the {ExecutionContext}.
+ */
 export interface IExecutionContextManager {
+    /**
+     * Gets the current execution context
+     */
     readonly current: ExecutionContext;
 
+    /**
+     * Set the current execution context for a tenant and possibly claims.
+     * @param {TenantId} tenantId Tenant to set for.
+     * @param [claims] Optional claims to set for.
+     * @returns {ExecutionContext} The execution context that was set.
+     */
     currentFor(tenantId: TenantId, claims?: Claims): ExecutionContext;
 }
-
