201e603b293a7b6a8d17b8e635a4d6b7e91db724
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-10 17:39:25 +0200

    Update Samples/Basic

--- Samples/Basic/MyEventHandler.ts
+++ Samples/Basic/MyEventHandler.ts
@@ -1,13 +1,13 @@
-import { event, EventContext } from '@dolittle/sdk.events';
-import { eventHandler } from '@dolittle/sdk.events.handling';
+import { EventContext } from '@dolittle/sdk.events';
+import { eventHandler, handles } from '@dolittle/sdk.events.handling';
 import { MyEvent } from './MyEvent';
 
 
-@eventHandler('a27074a7-5b01-43c9-b4f0-c1d59668d841')
+@eventHandler('a27074a7-5b01-43c9-b4f0-c1d59668d844')
 export class MyEventHandler {
 
-    @event(MyEvent)
-    handle(event: MyEvent, context: EventContext) {
-        console.log('Handling event');
+    @handles(MyEvent)
+    myEvent(event: MyEvent, context: EventContext) {
+        console.log('Handling event', event, event instanceof MyEvent);
     }
 }
--- Samples/Basic/index.ts
+++ Samples/Basic/index.ts
@@ -1,6 +1,5 @@
 import { MyEvent } from './MyEvent';
 import { Client } from '@dolittle/sdk';
-import { MyEventHandler } from './MyEventHandler';
 
 import { Cancellation } from '@dolittle/sdk.services';
 import { ReverseCallClient } from '@dolittle/sdk.services';
@@ -12,15 +11,27 @@ import { guids, artifacts } from '@dolittle/sdk.protobuf';
 import { Artifact } from '@dolittle/sdk.artifacts';
 import { EventContext } from '@dolittle/sdk.events';
 
+//import './MyEventHandler';
+
 const client = Client
                     .for('7a6155dd-9109-4488-8f6f-c57fe4b65bfb')
+                    .configureLogging(_ => _.level = 'debug')
                     .withEventHandlers(_ => {
-                        _.from(MyEventHandler);
-                        _.for('a0be3ef0-113f-45a6-800e-061d375407c1', ehb => {
+                        _.for('a0be3ef0-113f-45a6-800e-061d375407c2', ehb => {
                             ehb.handle(MyEvent, (myEvent: MyEvent, context: EventContext) => {
                                 console.log('Handled and happy');
                             });
                         });
+                    })
+                    .withFilters(_ => {
+                        _.for('79e12ab3-2751-47e1-b959-d898dc4d6ee8', fb => {
+                            fb
+                                .private()
+                                .handle((event: any, context: EventContext) => {
+                                    console.log('Filtering event', event);
+                                    return false;
+                                });
+                        })
                     })
                     .build();
 client.executionContextManager.currentFor('900893e7-c4cc-4873-8032-884e965e4b97');
