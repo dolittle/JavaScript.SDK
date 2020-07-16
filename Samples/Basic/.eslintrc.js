7dce8d0b5198f0dc209473bfcc732759d9283eeb
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-16 08:06:03 +0200

    Simplify eslint for Samples/Basic

--- Samples/Basic/.eslintrc.js
+++ Samples/Basic/.eslintrc.js
@@ -2,15 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 module.exports = {
-    extends: '@dolittle/typescript',
-    root: true,
-    parserOptions: {
-        project: './Sources/*/tsconfig.json',
-        sourceType: 'module',
-        tsconfigRootDir: __dirname,
-    },
+    extends: '../../.eslintrc.js',
     rules: {
-        "@typescript-eslint/no-this-alias": ['error', { allowedNames: ['self']}],
         "no-restricted-globals": 'off'
     }
 };
