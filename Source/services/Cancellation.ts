506b636d5200e148a5297d33a239d7d8976b07ba
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-20 12:39:12 +0200

    Add CancellationSource

--- Source/services/Cancellation.ts
+++ Source/services/Cancellation.ts
@@ -1,8 +1,27 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Observable, NEVER } from 'rxjs';
+import { Observable, NEVER, Subject, ReplaySubject } from 'rxjs';
 
 export class Cancellation extends Observable<void> {
     static default: Cancellation = NEVER;
+}
+
+export class CancellationSource {
+    private _subject: Subject<void>;
+
+    constructor(...sources: Cancellation[]) {
+        this._subject = new ReplaySubject(1);
+        for (const source of sources) {
+            source.subscribe(this._subject);
+        }
+    }
+
+    cancel(): void {
+        this._subject.next();
+    }
+
+    get cancellation(): Cancellation {
+        return this._subject;
+    }
 }
