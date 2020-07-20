b4ed54870c340fa8861a5181a1c38defcc9c7d64
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-20 21:00:50 +0200

    Change API of EventStore

--- Samples/Basic/index.ts
+++ Samples/Basic/index.ts
@@ -9,26 +9,27 @@ import './MyEventHandler';
 const client = Client
                     .for('7a6155dd-9109-4488-8f6f-c57fe4b65bfb')
                     .configureLogging(_ => _.level = 'debug')
-                    .withEventHandlers(_ => {
-                        _.for('a0be3ef0-113f-45a6-800e-061d375407c2', ehb => {
-                            ehb.handle(MyEvent, async (myEvent: MyEvent, context: EventContext) => {
-                                return new Promise((resolve) => {
-                                    console.log('Start processing', myEvent);
-                                    setTimeout(() => {
-                                        console.log('Done processing', myEvent);
-                                        resolve();
-                                    }, 2000);
-                                });
-                            });
-                        });
-                    })
+                    // .withEventHandlers(_ => {
+                    //     _.for('a0be3ef0-113f-45a6-800e-061d375407c2', ehb => {
+                    //         ehb.handle(MyEvent, async (myEvent: MyEvent, context: EventContext) => {
+                    //             return new Promise((resolve) => {
+                    //                 console.log('Start processing', myEvent);
+                    //                 setTimeout(() => {
+                    //                     console.log('Done processing', myEvent);
+                    //                     resolve();
+                    //                 }, 2000);
+                    //             });
+                    //         });
+                    //     });
+                    // })
                     .withFilters(_ => {
                         _.for('79e12ab3-2751-47e1-b959-d898dc4d6ee8', fb => {
                             fb
                                 .private()
-                                .handle(async (event: any, context: EventContext) => {
-                                    console.log('Filtering event', event);
-                                    return true;
+                                .handle((event: any, context: EventContext) => {
+                                    return new Promise((resolve, reject) => {
+                                        console.log('Filtering event', event);
+                                    });
                                 });
                         });
                     })
@@ -39,5 +40,5 @@ const event = new MyEvent();
 event.anInteger = 42;
 event.aString = 'Forty two';
 client.eventStore.commit(event, 'd8cb7301-4bec-4451-a72b-2db53c6dc05d');
-client.eventStore.commit(event, 'd8cb7301-4bec-4451-a72b-2db53c6dc05d');
-client.eventStore.commit(event, 'd8cb7301-4bec-4451-a72b-2db53c6dc05d');
+// client.eventStore.commit(event, 'd8cb7301-4bec-4451-a72b-2db53c6dc05d');
+// client.eventStore.commit(event, 'd8cb7301-4bec-4451-a72b-2db53c6dc05d');
--- Source/events/EventStore.ts
+++ Source/events/EventStore.ts
@@ -6,30 +6,21 @@ import { ArtifactId, Artifact, IArtifacts } from '@dolittle/sdk.artifacts';
 import { IExecutionContextManager } from '@dolittle/sdk.execution';
 
 import { IEventStore } from './IEventStore';
-import { CommittedEvent } from './CommittedEvent';
 import { CommittedEvents } from './CommittedEvents';
 import { CommitEventsResponse } from './CommitEventsResponse';
 import { EventSourceId } from './EventSourceId';
-import { EventsAndArtifactsAreNotTheSameSize } from './EventsAndArtifactsAreNotTheSameSize';
 
 import { EventStoreClient } from '@dolittle/runtime.contracts/Runtime/Events/EventStore_grpc_pb';
-import { UncommittedEvent } from '@dolittle/runtime.contracts/Runtime/Events/Uncommitted_pb';
 import { CommitEventsRequest } from '@dolittle/runtime.contracts/Runtime/Events/EventStore_pb';
 
-import { Guid } from '@dolittle/rudiments';
-import { InconsistentUseOfArrayForEventsAndArtifacts } from './InconsistentUseOfArrayForEventsAndArtifacts';
 
 import { EventConverters } from './EventConverters';
 
-import { ServiceError } from 'grpc';
-
 import { Logger } from 'winston';
 
-import { MissingEventsFromRuntime } from './MissingEventsFromRuntime';
-
 import { Cancellation, reactiveUnary } from '@dolittle/sdk.services';
-import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UncomittedEvent } from './UncomittedEvent';
 
 /**
  * Represents an implementation of {IEventStore}
@@ -43,64 +34,49 @@ export class EventStore implements IEventStore {
     }
 
     /** @inheritdoc */
-    async commit(event: any, eventSourceId: EventSourceId, artifactId?: string | string[] | Artifact | ArtifactId, cancellation?: Cancellation): Promise<CommitEventsResponse>;
-    async commit(events: any[], eventSourceId: EventSourceId, artifactIds?: string | string[] | Artifact[] | ArtifactId[], cancellation?: Cancellation): Promise<CommitEventsResponse>;
-    async commit(input: any | any[], eventSourceId: EventSourceId, inputId?: string | string[] | Artifact | Artifact[] | ArtifactId | ArtifactId[], cancellation: Cancellation = Cancellation.default): Promise<CommitEventsResponse> {
-        this._logger.debug('Commit:', input);
-        return this.commitInternal(false, input, eventSourceId, inputId);
+    commit(event: any, eventSourceId: EventSourceId, artifact?: Artifact | ArtifactId, cancellation?: Cancellation): Promise<CommitEventsResponse>;
+    commit(events: UncomittedEvent[], cancellation?: Cancellation): Promise<CommitEventsResponse>;
+    commit(eventOrEvents: any, eventSourceIdOrCancellation?: any, artifact?: Artifact | ArtifactId, cancellation?: Cancellation): Promise<CommitEventsResponse> {
+        if (arguments.length < 3) {
+            return this.commitInternal(eventOrEvents, eventSourceIdOrCancellation);
+        } else {
+            const events: UncomittedEvent[] = [{
+                content: eventOrEvents,
+                eventSourceId: eventSourceIdOrCancellation,
+                artifact,
+                public: false,
+            }];
+            return this.commitInternal(events, cancellation);
+        }
     }
-
 
     /** @inheritdoc */
-    commitPublic(event: any, eventSourceId: EventSourceId, artifactId?: Artifact | ArtifactId, cancellation?: Cancellation): Promise<CommitEventsResponse>;
-    commitPublic(events: any[], eventSourceId: EventSourceId, artifactIds?: Artifact[] | ArtifactId[], cancellation?: Cancellation): Promise<CommitEventsResponse>;
-    commitPublic(input: any | any[], eventSourceId: EventSourceId, inputId?: Artifact | Artifact[] | ArtifactId | ArtifactId[], cancellation: Cancellation = Cancellation.default): Promise<CommitEventsResponse> {
-        this._logger.debug('Commit public:', input);
-        return this.commitInternal(true, input, eventSourceId, inputId);
+    commitPublic(event: any, eventSourceId: EventSourceId, artifact?: Artifact | ArtifactId, cancellation?: Cancellation): Promise<CommitEventsResponse> {
+        const events: UncomittedEvent[] = [{
+            content: event,
+            eventSourceId,
+            artifact,
+            public: true,
+        }];
+        return this.commitInternal(events, cancellation);
     }
 
-    private async commitInternal(isPublic: boolean, input: any | any[], eventSourceId: EventSourceId, inputId?: string | string[] | Artifact | Artifact[] | ArtifactId | ArtifactId[]): Promise<CommitEventsResponse> {
-        const uncommittedEvents: UncommittedEvent[] = [];
-        const isEventArray = Array.isArray(input);
-        const isArtifactArray = Array.isArray(inputId);
-
-        this.throwIfEventAndArtifactTypesAreInconsistentOnSingleOrArray(isEventArray, isArtifactArray, input, inputId);
-
-        if (isEventArray && inputId) {
-            this.throwIfEventsArrayAndArtifactsArrayAreNotEqualInLength(inputId, input);
-
-            input.forEach((event: any, index: number) => {
-                const artifactOrId = (inputId as [])[index];
-                uncommittedEvents.push(EventConverters.getUncommittedEventFrom(event, eventSourceId, this._artifacts.resolveFrom(input, artifactOrId), false));
-            });
-        } else {
-            uncommittedEvents.push(EventConverters.getUncommittedEventFrom(input, eventSourceId, this._artifacts.resolveFrom(input, inputId as any), false));
-        }
+    private async commitInternal(events: UncomittedEvent[], cancellation = Cancellation.default): Promise<CommitEventsResponse> {
+        const uncommittedEvents = events.map(event =>
+            EventConverters.getUncommittedEventFrom(
+                event.content,
+                event.eventSourceId,
+                this._artifacts.resolveFrom(event.content, event.artifact),
+                !!event.public));
 
         const request = new CommitEventsRequest();
         request.setCallcontext(callContexts.toProtobuf(this._executionContextManager.current));
         request.setEventsList(uncommittedEvents);
-
-        const cancellation = new Subject<void>();
 
         return reactiveUnary(this._eventStoreClient, this._eventStoreClient.commit, request, cancellation)
             .pipe(map(response => {
                 const committedEvents = new CommittedEvents(...response.getEventsList().map(event => EventConverters.toSDK(event)));
                 return new CommitEventsResponse(committedEvents, failures.toSDK(response.getFailure()));
             })).toPromise();
-    }
-
-    private throwIfEventsArrayAndArtifactsArrayAreNotEqualInLength(inputId: string | string[] | Artifact | Artifact[] | Guid | ArtifactId[] | undefined, input: any) {
-        if (inputId && Array.isArray(inputId)) {
-            if (input.length !== inputId.length) {
-                throw new EventsAndArtifactsAreNotTheSameSize(input.length, inputId.length);
-            }
-        }
-    }
-
-    private throwIfEventAndArtifactTypesAreInconsistentOnSingleOrArray(isEventArray: boolean, isArtifactArray: boolean, input: any, inputId: string | string[] | Artifact | Artifact[] | Guid | ArtifactId[] | undefined) {
-        if (isEventArray !== isArtifactArray) {
-            throw new InconsistentUseOfArrayForEventsAndArtifacts(input, inputId);
-        }
     }
 }
