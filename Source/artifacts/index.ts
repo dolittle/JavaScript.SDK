459ce492ec5ec548bef254d087ac4f51456420cd
Sindre Wilting <sindrewilting@gmail.com>
2020-08-27 14:06:15 +0200

    Export named instead of bundled

--- Source/artifacts/index.ts
+++ Source/artifacts/index.ts
@@ -1,16 +1,16 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export * from './GenerationCannotBeLessThanZero';
-export * from './Generation';
-export * from './ArtifactId';
-export * from './Artifact';
-export * from './UnknownType';
-export * from './UnknownArtifact';
-export * from './UnableToResolveArtifact';
-export * from './ArtifactAssociation';
-export * from './ArtifactsFromDecorators';
-export * from './ArtifactsBuilder';
-export * from './IArtifacts';
-export * from './Artifacts';
-export * from './ArtifactMap';
+export { GenerationCannotBeLessThanZero } from './GenerationCannotBeLessThanZero';
+export { Generation } from './Generation';
+export { ArtifactId } from './ArtifactId';
+export { Artifact, artifact } from './Artifact';
+export { UnknownType } from './UnknownType';
+export { UnknownArtifact } from './UnknownArtifact';
+export { UnableToResolveArtifact } from './UnableToResolveArtifact';
+export { ArtifactAssociation } from './ArtifactAssociation';
+export { ArtifactsFromDecorators } from './ArtifactsFromDecorators';
+export { ArtifactsBuilder } from './ArtifactsBuilder';
+export { IArtifacts } from './IArtifacts';
+export { Artifacts } from './Artifacts';
+export { ArtifactMap } from './ArtifactMap';
