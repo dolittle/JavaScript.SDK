126739f5aa25a297d58e48819b5fc8a74e211536
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-09-03 08:41:47 +0200

    Fix basic sample

--- Samples/Basic/MyEventHandler.ts
+++ Samples/Basic/MyEventHandler.ts
@@ -5,7 +5,7 @@ import { eventHandler, handles } from '@dolittle/sdk.events.handling';
 import { MyEvent } from './MyEvent';
 
 
-@eventHandler('a27074a7-5b01-43c9-b4f0-c1d59668d844', { inScope: '406d6473-7cc9-44a6-a55f-775c1021d957' })
+@eventHandler('a27074a7-5b01-43c9-b4f0-c1d59668d844')
 export class MyEventHandler {
 
     @handles(MyEvent)
