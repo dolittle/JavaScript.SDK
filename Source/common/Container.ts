ac55dbfce4b0831d382e7d72d91c6505cb1395d9
Sindre Wilting <sindrewilting@gmail.com>
2020-09-30 09:48:10 +0200

    Fix import ordering in commonn

--- Source/common/Container.ts
+++ Source/common/Container.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { Constructor } from '@dolittle/types';
 import { IContainer } from './IContainer';
 import { DefaultContainerDoesNotSupportConstructorArguments } from './DefaultContainerDoesNotSupportConstructorArguments';
-import { Constructor } from '@dolittle/types';
 
 /**
  * Represents an implementation of {@link IContainer}.
--- Source/common/index.ts
+++ Source/common/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+export { DefaultContainerDoesNotSupportConstructorArguments } from './DefaultContainerDoesNotSupportConstructorArguments';
 export { IContainer } from './IContainer';
 export { Container } from './Container';
-export { DefaultContainerDoesNotSupportConstructorArguments } from './DefaultContainerDoesNotSupportConstructorArguments';
