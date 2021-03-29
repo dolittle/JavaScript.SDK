38e552c3265560c940131f4928e1902c95c1de99
Joel Hoisko <joel.hoisko@gmail.com>
2021-03-29 11:18:07 +0200

    Add sample for deleting a readmodel

--- Samples/Tutorials/Projections/index.ts
+++ Samples/Tutorials/Projections/index.ts
@@ -4,44 +4,50 @@
 
 import { Client } from '@dolittle/sdk';
 import { TenantId } from '@dolittle/sdk.execution';
+import { ProjectionResult } from '@dolittle/sdk.projections';
 import { Chef } from './Chef';
+import { ChefFired } from './ChefFired';
 import { DishPrepared } from './DishPrepared';
 
 const client = Client
     .forMicroservice('f39b1f61-d360-4675-b859-53c05c87c0e6')
-    .withEventTypes(eventTypes =>
-        eventTypes.register(DishPrepared))
+    .withEventTypes(eventTypes => {
+        eventTypes.register(DishPrepared);
+        eventTypes.register(ChefFired);
+    })
     .withProjections(projections => {
         projections.createProjection('4a4c5b13-d4dd-4665-a9df-27b8e9b2054c')
             .forReadModel(Chef)
             .on(DishPrepared, _ => _.keyFromProperty('Chef'), (chef, event, ctx) => {
+                console.log(`Handling event ${JSON.stringify(event)} and read model ${JSON.stringify(chef)}`);
+                chef.name = event.Chef;
                 chef.dishes.push(event.Dish);
                 return chef;
-            });
-        //    projections.createProjection('4a4c5b13-d4dd-4665-a9df-27b8e9b2054c')
-        //         .forReadModel({ name: '', dishes: [] as string[] })
-        //         .on(DishPrepared, _ => _.keyFromProperty('Chef'), (chef, event, ctx) => {
-        //             chef.dishes.push(event.Dish);
-        //             return chef;
-        //         });
+            })
+            .on(ChefFired, _ => _.keyFromProperty('Chef'), (chef, event, ctx) => {
+                console.log(`Firing ${chef.name}`);
+                return ProjectionResult.delete;
+            })
         })
     .build();
-
-// client.projections.Get(Chef, 'Mr. Taco');
-// // client.projections.Get({ chefName: '', dishes: [] }, 'Mr. Taco');
-// // client.projections.Get(new Chef(), 'Mr. Taco');
-// client.projections.Get('686eb643-27e8-4f3b-8036-1ce960ed1e6a', 'Mr. Taco');
 
 const beanBlaster = new DishPrepared('Bean Blaster Taco', 'Mr. Taco');
 const avocadoArtillery = new DishPrepared('Avocado Artillery Tortilla', 'Mr. Taco');
 const chiliCannon = new DishPrepared('Chili Cannon Wrap', 'Ms. TexMex');
+const mrTacoFired = new ChefFired('Mr. Taco');
 
-client.eventStore
-    .forTenant(TenantId.development)
-    .commit(beanBlaster, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
-client.eventStore
-    .forTenant(TenantId.development)
-    .commit(avocadoArtillery, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
-client.eventStore
-    .forTenant(TenantId.development)
-    .commit(chiliCannon, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+(async () => {
+    await client.eventStore
+        .forTenant(TenantId.development)
+        .commit(beanBlaster, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await client.eventStore
+        .forTenant(TenantId.development)
+        .commit(avocadoArtillery, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await client.eventStore
+        .forTenant(TenantId.development)
+        .commit(chiliCannon, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+
+    await client.eventStore
+        .forTenant(TenantId.development)
+        .commit(mrTacoFired, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+})();
