83dc9c57d3f45733cf85b3287b8a861a116312c5
Joel Hoisko <joel.hoisko@gmail.com>
2021-03-30 10:51:04 +0200

    Fix typo

--- Source/projections/Builder/ProjectionsBuilderCallback.ts
+++ Source/projections/Builder/ProjectionsBuilderCallback.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { ProjectionsBuilder } from './Projectio';
+import { ProjectionsBuilder } from './';
 
 /**
  * Defines the callback signature used for building projections.
