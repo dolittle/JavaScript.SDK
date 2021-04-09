5b438ea06e938a7736214b886f6ab2e69c3f2ad0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-04-09 13:39:08 +0200

    Update Projections tutorial

---
+++ Samples/Tutorials/Projections/DishCounter.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Sample code for the tutorial at https://dolittle.io/tutorials/projections/typescript/
+
+import { ProjectionContext, projection, on } from '@dolittle/sdk.projections';
+import { DishPrepared } from './DishPrepared';
+
+@projection('98f9db66-b6ca-4e5f-9fc3-638626c9ecfa')
+export class DishCounter {
+    numberOfTimesPrepared: number = 0;
+
+    @on(DishPrepared, _ => _.keyFromProperty('Dish'))
+    dishPrepared(event: DishPrepared, projectionContext: ProjectionContext) {
+        this.numberOfTimesPrepared ++;
+    }
+}
--- Samples/Tutorials/Projections/index.ts
+++ Samples/Tutorials/Projections/index.ts
@@ -1,74 +1,47 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-// Sample code for the tutorial at https://dolittle.io/tutorials/getting-started/projections/
+// Sample code for the tutorial at https://dolittle.io/tutorials/projections/typescript/
 
 import { Client } from '@dolittle/sdk';
 import { TenantId } from '@dolittle/sdk.execution';
-import { ProjectionResult } from '@dolittle/sdk.projections';
-import { Menu } from './Menu';
-import { Chef } from './Chef';
-import { ChefFired } from './ChefFired';
 import { DishPrepared } from './DishPrepared';
-import { ScopeId } from '@dolittle/sdk.events';
+import { DishHandler } from './DishHandler';
+import { DishCounter } from './DishCounter';
+import { Chef } from './Chef';
 
 const client = Client
     .forMicroservice('f39b1f61-d360-4675-b859-53c05c87c0e6')
-    .withEventTypes(eventTypes => {
-        eventTypes.register(DishPrepared);
-        eventTypes.register(ChefFired);
-    })
-    .withProjections(projections => {
-        projections.createProjection('4a4c5b13-d4dd-4665-a9df-27b8e9b2054c')
+    .withEventTypes(eventTypes =>
+        eventTypes.register(DishPrepared))
+    .withEventHandlers(builder =>
+        builder.register(DishHandler))
+    .withProjections(builder => {
+        builder.register(DishCounter);
+
+        builder.createProjection('0767bc04-bc03-40b8-a0be-5f6c6130f68b')
             .forReadModel(Chef)
-            .on(DishPrepared, _ => _.keyFromProperty('Chef'), (chef, event, ctx) => {
-                console.log(`Handling event ${JSON.stringify(event)} and read model ${JSON.stringify(chef)}`);
+            .on(DishPrepared, _ => _.keyFromProperty('Chef'), (chef, event, projectionContext) => {
                 chef.name = event.Chef;
-                chef.dishes.push(event.Dish);
+                if (!chef.dishes.includes(event.Dish)) chef.dishes.push(event.Dish);
                 return chef;
-            })
-            .on(ChefFired, _ => _.keyFromProperty('Chef'), (chef, event, ctx) => {
-                console.log(`Firing ${chef.name}`);
-                return ProjectionResult.delete;
             });
-        projections.register(Menu);
     })
     .build();
 
-const beanBlaster = new DishPrepared('Bean Blaster Taco', 'Mr. Taco');
-const avocadoArtillery = new DishPrepared('Avocado Artillery Tortilla', 'Mr. Taco');
-const chiliCannon = new DishPrepared('Chili Cannon Wrap', 'Ms. TexMex');
-const mrTacoFired = new ChefFired('Mr. Taco');
-
 (async () => {
-    client.eventStore
-        .forTenant(TenantId.development)
-        .commit(beanBlaster, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
-    client.eventStore
-        .forTenant(TenantId.development)
-        .commit(avocadoArtillery, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
-    client.eventStore
-        .forTenant(TenantId.development)
-        .commit(chiliCannon, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    const eventStore = client.eventStore.forTenant(TenantId.development);
 
-    client.eventStore
-        .forTenant(TenantId.development)
-        .commit(mrTacoFired, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await eventStore.commit(new DishPrepared('Bean Blaster Taco', 'Mr. Taco'), 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await eventStore.commit(new DishPrepared('Bean Blaster Taco', 'Mrs. Tex Mex'), 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await eventStore.commit(new DishPrepared('Avocado Artillery Tortilla', 'Mr. Taco'), 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
+    await eventStore.commit(new DishPrepared('Chili Canon Wrap', 'Mrs. Tex Mex'), 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
 
-    const {state: menu} = await client.projections.forTenant(TenantId.development).get(Menu, 'bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9');
-    console.log('Got menu', menu);
+    setTimeout(async () => {
+        for (const [dish, { state: counter }] of await client.projections.forTenant(TenantId.development).getAll(DishCounter)) {
+            console.log(`The kitchen has prepared ${dish} ${counter.numberOfTimesPrepared} times`);
+        }
 
-    console.log('Getting all chefs');
-    for (const [, { key, state: chef }] of await client.projections.forTenant(TenantId.development).getAll(Chef)) {
-        console.log(`Found chef ${key}`, chef);
-    }
-
-    console.log('Getting all chefs but specify ids');
-    for (const [, { key, state: chef }] of await client.projections.forTenant(TenantId.development).getAll(Chef, '4a4c5b13-d4dd-4665-a9df-27b8e9b2054d')) {
-        console.log(`Found chef ${key}`, chef);
-    }
-
-    console.log('Getting all chefs with no type');
-    for (const [, { key, state: chef }] of await client.projections.forTenant(TenantId.development).getAll('4a4c5b13-d4dd-4665-a9df-27b8e9b2054c', ScopeId.default)) {
-        console.log(`Found chef ${key}`, chef);
-    }
+        const chef = await client.projections.forTenant(TenantId.development).get<Chef>(Chef, 'Mrs. Tex Mex');
+        console.log(`${chef.key} has prepared ${chef.state.dishes}`);
+    }, 1000);
 })();
