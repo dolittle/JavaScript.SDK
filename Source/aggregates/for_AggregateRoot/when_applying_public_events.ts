ebeee3a3d36d7105b6e424dd7995b947f33597e7
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-11-04 11:31:16 +0100

    Fix spec

--- Source/aggregates/for_AggregateRoot/when_applying_public_events.ts
+++ Source/aggregates/for_AggregateRoot/when_applying_public_events.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Generation } from '@dolittle/sdk.artifacts';
-import { EventSourceId, EventType, EventTypeId } from '@dolittle/sdk.events';
+import { EventSourceId, EventType, EventTypeId, EventTypes } from '@dolittle/sdk.events';
 import { AggregateRoot } from '../AggregateRoot';
 
 describe('when applying public events', () => {
@@ -10,6 +10,9 @@ describe('when applying public events', () => {
 
     const eventType = new EventType(EventTypeId.from('7b078c73-5843-434e-9b4d-ecae4e91469e'), Generation.first);
     const aggregateRoot = new AggregateRoot(EventSourceId.from('8f58f9d5-fded-49f3-8334-a3c1f447d4da'));
+    const eventTypes = new EventTypes();
+    aggregateRoot.eventTypes = eventTypes;
+
     aggregateRoot.applyPublic(event, eventType);
 
     it('should have one event applied', () => aggregateRoot.appliedEvents.length.should.equal(1));
