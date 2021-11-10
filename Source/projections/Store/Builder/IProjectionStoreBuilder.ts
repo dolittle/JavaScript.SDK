3df311489a8c187fb79751303d4a8e52ed858ecd
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-11-10 11:12:15 +0100

    Introduce IProjectionStoreBuilder

---
+++ Source/projections/Store/Builder/IProjectionStoreBuilder.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+import { TenantIdLike } from '@dolittle/sdk.execution';
+import { IProjectionStore } from '..';
+
+/**
+ * Defines a builder for {@link IProjectionStore}.
+ */
+
+export abstract class IProjectionStoreBuilder {
+    /**
+     * Build an {@link IProjectionStore} for the given tenant.
+     * @param { TenantIdLike } tenantId The tenant id.
+     * @returns {IProjectionStore} The projection store.
+     */
+    abstract forTenant(tenantId: TenantIdLike): IProjectionStore;
+}
