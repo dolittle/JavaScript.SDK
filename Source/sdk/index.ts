e5e78885b7b146f1851a85a5fe422747d04d9ac3
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-07 21:02:11 +0100

    Fix build issues

--- Source/sdk/index.ts
+++ Source/sdk/index.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import '@dolittle/sdk.execution';
 import '@dolittle/sdk.protobuf';
 
-export { EventStoreBuilderCallback } from './EventStoreBuilderCallback';
 export { IDolittleClient } from './IDolittleClient';
 export { DolittleClient } from './DolittleClient';
-export { DolittleClientBuilder } from './DolittleClientBuilder';
