36fbe2247a18a00362bec5afde3e454cc8f8ee31
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-11 22:55:57 +0100

    Fix eventHorizon exports

--- Source/eventHorizon/index.ts
+++ Source/eventHorizon/index.ts
@@ -1,29 +1,4 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export { CallbackShouldBeFunction } from './CallbackShouldBeFunction';
-export { ConsentId } from './ConsentId';
-export { EventHorizons } from './EventHorizons';
-export { EventHorizonSubscriptionFailed } from './EventHorizonSubscriptionFailed';
-export { IEventHorizons } from './IEventHorizons';
-export { ISubscriptionBuilderForConsumerScope } from './ISubscriptionBuilderForConsumerScope';
-export { ISubscriptionBuilderForProducerMicroservice } from './ISubscriptionBuilderForProducerMicroservice';
-export { ISubscriptionBuilderForProducerPartition } from './ISubscriptionBuilderForProducerPartition';
-export { ISubscriptionBuilderForProducerStream } from './ISubscriptionBuilderForProducerStream';
-export { ISubscriptionsBuilder } from './ISubscriptionsBuilder';
-export { ISubscriptionsBuilderForConsumerTenant } from './ISubscriptionsBuilderForConsumerTenant';
-export { Subscription } from './Subscription';
-export { SubscriptionBuilderForConsumerScope } from './SubscriptionBuilderForConsumerScope';
-export { SubscriptionBuilderForProducerMicroservice } from './SubscriptionBuilderForProducerMicroservice';
-export { SubscriptionBuilderForProducerPartition } from './SubscriptionBuilderForProducerPartition';
-export { SubscriptionBuilderForProducerStream } from './SubscriptionBuilderForProducerStream';
-export { SubscriptionBuilderForProducerTenant } from './SubscriptionBuilderForProducerTenant';
-export { SubscriptionBuilderMethodAlreadyCalled } from './SubscriptionBuilderMethodAlreadyCalled';
-export { SubscriptionCallbacks, SubscriptionCallbackArguments, SubscriptionCompleted, SubscriptionFailed, SubscriptionSucceeded } from './SubscriptionCallbacks';
-export { SubscriptionDefinitionIncomplete } from './SubscriptionDefinitionIncomplete';
-export { SubscriptionDoesNotExist } from './SubscriptionDoesNotExist';
-export { SubscriptionResponse } from './SubscriptionResponse';
-export { SubscriptionsBuilder } from './SubscriptionsBuilder';
-export { SubscriptionsBuilderCallback } from './SubscriptionsBuilderCallback';
-export { SubscriptionsBuilderForConsumerTenant } from './SubscriptionsBuilderForConsumerTenant';
-export { TenantWithSubscriptions } from './TenantWithSubscriptions';
+export * from './_exports';
