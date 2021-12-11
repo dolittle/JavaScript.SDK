969d56eec07609b3f12ced77ec38f05daadac7ec
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-12 00:24:12 +0100

    Fix sdk exports

---
+++ Source/sdk/_exports.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export { CannotConnectDolittleClientMultipleTimes } from './CannotConnectDolittleClientMultipleTimes';
+export { CannotUseUnconnectedDolittleClient } from './CannotUseUnconnectedDolittleClient';
+export { DolittleClient } from './DolittleClient';
+export { DolittleClientConfiguration } from './DolittleClientConfiguration';
+export { IDolittleClient } from './IDolittleClient';
+export { VersionInfo } from './VersionInfo';
--- Source/sdk/index.ts
+++ Source/sdk/index.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+export * from './_exports';
+
 import '@dolittle/sdk.execution';
 import '@dolittle/sdk.protobuf';
-
-export { IDolittleClient } from './IDolittleClient';
-export { DolittleClient } from './DolittleClient';
