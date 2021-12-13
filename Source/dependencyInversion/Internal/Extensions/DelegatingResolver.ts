d8ce1b38b9994bd0f17fda488613cc0cb6b06372
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-13 22:00:40 +0100

    Remove old debugging console logger.

--- Source/dependencyInversion/Internal/Extensions/DelegatingResolver.ts
+++ Source/dependencyInversion/Internal/Extensions/DelegatingResolver.ts
@@ -21,7 +21,6 @@ export class DelegatingResolver extends IDynamicResolver {
 
     /** @inheritdoc */
     bindUnknownService(serviceIdentifier: interfaces.ServiceIdentifier<unknown>, bindings: Bindings): void {
-        console.log('Delegating called with', serviceIdentifier);
         if (this._services.has(serviceIdentifier)) {
             // TODO: Deal with ResolveAll here (probably easy with the context)
             bindings.bind(serviceIdentifier).toDynamicValue(() => this._services.get(serviceIdentifier));
