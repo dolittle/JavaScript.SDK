608d7db785982351347f2c53db3195a49d84fd6f
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-14 18:53:51 +0100

    Fix some forgotten exports.

--- Source/sdk/Internal/_exports.ts
+++ Source/sdk/Internal/_exports.ts
@@ -2,3 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export { ConnectConfiguration } from './ConnectConfiguration';
+export { ConnectionResult } from './ConnectionResult';
+export { ICanConnectToARuntime } from './ICanConnectToARuntime';
+export { RuntimeConnector } from './RuntimeConnector';
