22ccbb5431be5b7cb3bb057d4ec0d4245ccea720
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-14 20:19:00 +0100

    Make the module traverser A LOT faster.
    Only load modules referenced from the main, and stop when a module in
    the blacklist is encountered. Currently the blacklist is only the SDK,
    but it can be expanded upon to include all the packages we know don't
    expose any types we would like to register.

--- Source/sdk/Internal/Discovery/ModuleTraverser.ts
+++ Source/sdk/Internal/Discovery/ModuleTraverser.ts
@@ -10,9 +10,18 @@ type ObjectCallback = (object: any) => void;
  * Represents an implementation of {@link ICanTraverseModules}.
  */
 export class ModuleTraverser extends ICanTraverseModules {
+    /**
+     * A list of module paths that we know don't export any interesting types, and will therefore not be traversed.
+     * Note: all modules imported by these blacklisted modules will also not be traversed.
+     */
+    static readonly blacklistPaths: readonly string[] = [
+        'node_modules/@dolittle',
+        'JavaScript.SDK/Source',
+    ];
+
     /** @inheritdoc */
     forEachClass(callback: ClassCallback): void {
-        this.traverseLoadedModules((object) => {
+        this.traverseLoadedModulesFromMain((object) => {
             if (typeof object !== 'function') {
                 return;
             }
@@ -25,24 +34,40 @@ export class ModuleTraverser extends ICanTraverseModules {
         });
     }
 
-    private traverseLoadedModules(callback: ObjectCallback): void {
-        if (typeof require.cache !== 'object') {
+    private traverseLoadedModulesFromMain(callback: ObjectCallback): void {
+        if (typeof require.main !== 'object') {
             return;
         }
 
-        for (const moduleId in require.cache) {
-            const module = require.cache[moduleId];
+        const set = new Set<string>();
+        this.traverseLoadedModuleRecursively(require.main, set, callback);
+    }
 
-            if (module === undefined || module.loaded === false) {
-                continue;
+    private traverseLoadedModuleRecursively(module: NodeJS.Module, checked: Set<string>, callback: ObjectCallback): void {
+        if (module.id === undefined || checked.has(module.id)) {
+            return;
+        }
+        checked.add(module.id);
+
+        for (const blacklistPath of ModuleTraverser.blacklistPaths) {
+            if (module.path.includes(blacklistPath)) {
+                return;
             }
+        }
 
+        if (module.loaded === true && module.exports !== undefined) {
             this.traverseExportsRecursively(module.exports, [], callback);
+        }
+
+        if (Array.isArray(module.children)) {
+            for (const child of module.children) {
+                this.traverseLoadedModuleRecursively(child, checked, callback);
+            }
         }
     }
 
     private traverseExportsRecursively(object: any, stack: any[], callback: ObjectCallback): void {
-        if (stack.includes(object)) {
+        if (object === undefined || stack.includes(object)) {
             return;
         }
 
