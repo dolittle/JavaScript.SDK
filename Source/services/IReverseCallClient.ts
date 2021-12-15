964b2a0c941c881796cf57bc2164065c77782389
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-12-15 18:10:08 +0100

    Make the ReverseCallClient be an Observable so we can more easily compose it.

--- Source/services/IReverseCallClient.ts
+++ Source/services/IReverseCallClient.ts
@@ -1,8 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { Observable } from 'rxjs';
+
 import { ExecutionContext } from '@dolittle/sdk.execution';
-import { PartialObserver, Subscribable, Unsubscribable } from 'rxjs';
 
 /**
  * Defines a reverse call client callback to handle a request coming from the server to the client.
@@ -12,11 +13,4 @@ export type ReverseCallCallback<TRequest, TResponse> = (request: TRequest, execu
 /**
  * Defines a client for reverse calls coming from the server to the client.
  */
-export abstract class IReverseCallClient<TConnectResponse> implements Subscribable<TConnectResponse> {
-    abstract subscribe(observer?: PartialObserver<TConnectResponse>): Unsubscribable;
-    abstract subscribe(next: null | undefined, error: null | undefined, complete: () => void): Unsubscribable;
-    abstract subscribe(next: null | undefined, error: (error: any) => void, complete?: () => void): Unsubscribable;
-    abstract subscribe(next: (value: TConnectResponse) => void, error: null | undefined, complete: () => void): Unsubscribable;
-    abstract subscribe(next?: (value: TConnectResponse) => void, error?: (error: any) => void, complete?: () => void): Unsubscribable;
-    abstract subscribe(next?: any, error?: any, complete?: any): Unsubscribable;
-}
+export abstract class IReverseCallClient<TConnectResponse> extends Observable<TConnectResponse> {}
