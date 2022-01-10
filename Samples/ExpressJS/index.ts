120356124550d697df9c7cb5c7bd020c34f2f9db
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-01-10 21:33:37 +0100

    Move Express to ExpressJS to avoid collision with the actual package.

---
+++ Samples/ExpressJS/index.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// Sample code for the tutorial at https://dolittle.io/docs/tutorials/projections/
+
+// import { DolittleClient } from '@dolittle/sdk';
+// import { TenantId } from '@dolittle/sdk.execution';
+// import { setTimeout } from 'timers/promises';
+
+// import { Chef } from './Chef';
+// import { DishCounter } from './DishCounter';
+// import { DishPrepared } from './DishPrepared';
+
+// (async () => {
+//     const client = await DolittleClient
+//         .setup(builder => builder
+//             .withProjections(builder => {
+//                 builder.createProjection('0767bc04-bc03-40b8-a0be-5f6c6130f68b')
+//                     .forReadModel(Chef)
+//                     .on(DishPrepared, _ => _.keyFromProperty('Chef'), (chef, event, projectionContext) => {
+//                         chef.name = event.Chef;
+//                         if (!chef.dishes.includes(event.Dish)) chef.dishes.push(event.Dish);
+//                         return chef;
+//                     });
+//             }))
+//         .connect();
+
+//     const eventStore = client.eventStore.forTenant(TenantId.development);
+
+//     await eventStore.commit(new DishPrepared('Bean Blaster Taco', 'Mr. Taco'), 'Dolittle Tacos');
+//     await eventStore.commit(new DishPrepared('Bean Blaster Taco', 'Mrs. Tex Mex'), 'Dolittle Tacos');
+//     await eventStore.commit(new DishPrepared('Avocado Artillery Tortilla', 'Mr. Taco'), 'Dolittle Tacos');
+//     await eventStore.commit(new DishPrepared('Chili Canon Wrap', 'Mrs. Tex Mex'), 'Dolittle Tacos');
+
+//     await setTimeout(1000);
+
+//     for (const [dish, { state: counter }] of await client.projections.forTenant(TenantId.development).getAll(DishCounter)) {
+//         console.log(`The kitchen has prepared ${dish} ${counter.numberOfTimesPrepared} times`);
+//     }
+
+//     const chef = await client.projections.forTenant(TenantId.development).get<Chef>(Chef, 'Mrs. Tex Mex');
+//     console.log(`${chef.key} has prepared ${chef.state.dishes}`);
+// })();
+
+import createApplication, { Request } from 'express';
+import { dolittle } from '@dolittle/sdk.extensions.express';
+
+const application = createApplication();
+
+application.use(dolittle());
+
+application.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = 8080;
+application.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
