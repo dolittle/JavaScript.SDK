0b73e6d9cad7853e665463ac94049a9b0e1e6e8f
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-01-13 10:06:52 +0100

    Use logger in embeddings

--- Samples/Tutorials/Embeddings/index.ts
+++ Samples/Tutorials/Embeddings/index.ts
@@ -14,10 +14,8 @@ import { Employee } from './Employee';
         .setup()
         .connect();
 
-    // wait for the registration to complete
-    await setTimeout(1000);
+    await setTimeout(2000);
 
-    // mock of the state from the external HR system
     const updatedEmployee = new Employee(
         'Mr. Taco',
         'Street Food Taco Truck');
@@ -25,20 +23,20 @@ import { Employee } from './Employee';
     await client.embeddings
         .forTenant(TenantId.development)
         .update(Employee, updatedEmployee.name, updatedEmployee);
-    console.log(`Updated ${updatedEmployee.name}`);
+    client.logger.info(`Updated ${updatedEmployee.name}`);
 
     const mrTaco = await client.embeddings
         .forTenant(TenantId.development)
         .get(Employee, 'Mr. Taco');
-    console.log(`Mr. Taco is now working at ${mrTaco.state.workplace}`);
+    client.logger.info(`Mr. Taco is now working at ${mrTaco.state.workplace}`);
 
     const allEmployeeNames = await client.embeddings
         .forTenant(TenantId.development)
         .getKeys(Employee);
-    console.log(`All current employees are ${allEmployeeNames}`);
+    client.logger.info(`All current employees are ${allEmployeeNames}`);
 
     await client.embeddings
         .forTenant(TenantId.development)
         .delete(Employee, updatedEmployee.name);
-    console.log(`Deleted ${updatedEmployee.name}`);
+    client.logger.info(`Deleted ${updatedEmployee.name}`);
 })();
