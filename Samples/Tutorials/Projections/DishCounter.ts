295e692f2806beb5c3956a5aaa1405e843848715
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-01 21:05:04 +0100

    Test with Projection sample

--- Samples/Tutorials/Projections/DishCounter.ts
+++ Samples/Tutorials/Projections/DishCounter.ts
@@ -3,13 +3,16 @@
 
 // Sample code for the tutorial at https://dolittle.io/tutorials/projections/typescript/
 
-import { ProjectionContext, projection, on } from '@dolittle/sdk.projections';
+import { ProjectionContext, projection, on, copyProjectionToMongoDB, convertToMongoDB, MongoDBConversion } from '@dolittle/sdk.projections';
 
 import { DishPrepared } from './DishPrepared';
 
 @projection('98f9db66-b6ca-4e5f-9fc3-638626c9ecfa')
+@copyProjectionToMongoDB()
 export class DishCounter {
+
     name: string = 'Unknown';
+    @convertToMongoDB(MongoDBConversion.Binary)
     numberOfTimesPrepared: number = 0;
 
     @on(DishPrepared, _ => _.keyFromProperty('Dish'))
