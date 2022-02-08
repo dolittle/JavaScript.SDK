53640cd7d82d2ce6fd7ae68f06931cfcb7b300b0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-08 21:56:39 +0100

    Add some specs for ProjectionClassBuilder copy to MongoDB decorators

---
+++ Source/projections/Builders/for_ProjectionClassBuilder/when_building/with_copies_to_mongodb/with_copy_to_mongodb.ts
@@ -0,0 +1,39 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { describeThis } from '@dolittle/typescript.testing';
+import { stubInterface } from 'ts-sinon';
+
+import { IEventTypes, ScopeId } from '@dolittle/sdk.events';
+import { IClientBuildResults } from '@dolittle/sdk.common';
+
+import { ProjectionId } from '../../../../ProjectionId';
+import { copyProjectionToMongoDB } from '../../../Copies/copyProjectionToMongoDBDecorator';
+import { on } from '../../../onDecorator';
+import { ProjectionDecoratedType } from '../../../ProjectionDecoratedType';
+import { ProjectionClassBuilder } from '../../../ProjectionClassBuilder';
+
+@copyProjectionToMongoDB()
+class Projection {
+    @on('32e2aaca-2374-42f0-bcb5-7fb19501e3da', _ => _.keyFromEventSource())
+    on() {}
+}
+
+describeThis(__filename, () => {
+    const decoratedType = new ProjectionDecoratedType(
+        ProjectionId.from('53cad890-5b8a-4146-acce-29dc2dc8c43e'),
+        ScopeId.from('28f6c232-cc11-493b-b032-398a5e651617'),
+        Projection);
+
+    const builder = new ProjectionClassBuilder(decoratedType);
+
+    const event_types = stubInterface<IEventTypes>();
+    const build_results = stubInterface<IClientBuildResults>();
+
+    const result = builder.build(event_types, build_results);
+
+    it('should return a projection', () => result!.should.not.be.undefined);
+    it('should set copy to MongoDB', () => result?.copies.mongoDB.shouldCopyToMongoDB.should.be.true);
+    it('should copy to a collection with the same name as the class', () => result?.copies.mongoDB.collectionName.value.should.equal(Projection.name));
+    it('should not have any conversions', () => result?.copies.mongoDB.conversions.should.be.empty);
+});
