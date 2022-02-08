e862f96d3a7251551001210e26f2e8c65c03db7b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-08 22:15:34 +0100

    Add specs for ProjectionBuilderForReadModel for MongoDB copies

--- Source/projections/Builders/for_ProjectionBuilderForReadModel/when_building/with_copies_to_mongodb/without_calling_copyToMongoDB.ts
+++ Source/projections/Builders/for_ProjectionBuilderForReadModel/when_building/with_copies_to_mongodb/without_calling_copyToMongoDB.ts
@@ -5,25 +5,25 @@ import { describeThis } from '@dolittle/typescript.testing';
 import { stubInterface } from 'ts-sinon';
 
 import { IEventTypes, ScopeId } from '@dolittle/sdk.events';
-import { IClientBuildResults } from '@dolittle/sdk.common';
+import { IClientBuildResults, IModelBuilder } from '@dolittle/sdk.common';
 
 import { ProjectionId } from '../../../../ProjectionId';
-import { on } from '../../../onDecorator';
-import { ProjectionDecoratedType } from '../../../ProjectionDecoratedType';
-import { ProjectionClassBuilder } from '../../../ProjectionClassBuilder';
-
-class Projection {
-    @on('32e2aaca-2374-42f0-bcb5-7fb19501e3da', _ => _.keyFromEventSource())
-    on() {}
-}
+import { ProjectionBuilder } from '../../../ProjectionBuilder';
+import { ProjectionBuilderForReadModel } from '../../../ProjectionBuilderForReadModel';
 
 describeThis(__filename, () => {
-    const decoratedType = new ProjectionDecoratedType(
-        ProjectionId.from('53cad890-5b8a-4146-acce-29dc2dc8c43e'),
-        ScopeId.from('28f6c232-cc11-493b-b032-398a5e651617'),
-        Projection);
 
-    const builder = new ProjectionClassBuilder(decoratedType);
+    const model_builder = stubInterface<IModelBuilder>();
+    const parent_builder = stubInterface<ProjectionBuilder>();
+
+    const builder = new ProjectionBuilderForReadModel(
+        ProjectionId.from('2640b8db-0391-4f99-ae3c-093ed191b00a'),
+        {},
+        ScopeId.from('87655996-d359-483e-a075-c7d833c4e572'),
+        model_builder,
+        parent_builder);
+
+    builder.on('cf566852-1a29-4f96-ba54-568c88e4a0e6', _ => _.keyFromEventSource(), _ => _);
 
     const event_types = stubInterface<IEventTypes>();
     const build_results = stubInterface<IClientBuildResults>();
