1e93ad6d1c8326a337aa1eaa511ba109e0de3fe6
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-02-09 21:05:14 +0100

    Change the sample again to use the service provider.

--- Samples/MongoDBProjections/index.ts
+++ Samples/MongoDBProjections/index.ts
@@ -7,6 +7,7 @@ import { DolittleClient } from '@dolittle/sdk';
 import { TenantId } from '@dolittle/sdk.execution';
 import { setTimeout } from 'timers/promises';
 import { DateTime } from 'luxon';
+import { Collection } from 'mongodb';
 
 import { DishCounter } from './DishCounter';
 import { DishPrepared } from './DishPrepared';
@@ -25,7 +26,7 @@ import { DishPrepared } from './DishPrepared';
 
     await setTimeout(1000);
 
-    const dishCounterCollection = client.resources.forTenant(TenantId.development).mongoDB.getDatabase().collection(DishCounter);
+    const dishCounterCollection = client.services.forTenant(TenantId.development).get(Collection.forReadModel(DishCounter));
 
     for (const { name, numberOfTimesPrepared, lastPrepared } of await dishCounterCollection.find().toArray()) {
         client.logger.info(`The kitchen has prepared ${name} ${numberOfTimesPrepared} times. The last time was ${lastPrepared}`);
